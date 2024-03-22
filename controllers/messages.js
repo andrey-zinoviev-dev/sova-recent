01d0b792a53e30f90efbb7c70f39af7a7b0ef316
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-03-23 01:48:08 +0300

    files are sent now

--- controllers/messages.js
+++ controllers/messages.js
@@ -29,7 +29,40 @@ const getMessagesOfUser = ((req, res, next) => {
     if(!foundConvo) {
       throw new Error("Сообщений нет");
     } else {
-      return res.status(200).send(foundConvo);
+      // const filteredMessages = foundConvo.messages.filter((message) => {
+      //   return message.files.length > 0;
+      // });
+      // console.log(filteredMessages);
+      Promise.all(foundConvo.messages.map((message) => {
+        if(message.files.length > 0) {
+          const readCommand = new GetObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Key: message.files[0].title,
+          });
+
+          return getSignedUrl(s3, readCommand, {
+            expiresIn: 90
+          })
+          .then((url) => {
+            message.files[0].path = url;
+            return message;
+          })
+        }
+        else {
+          return new Promise((resolve, reject) => {
+            resolve(message);
+          })
+          .then((data) => {
+            return data;
+          })
+        }
+      }))
+      .then((finalMessages) => {
+        // console.log(finalMessages);
+        return res.status(200).send(finalMessages);
+
+      })
+      // return res.status(200).send(foundConvo);
     }
   })
   .catch((err) => {
@@ -90,54 +123,10 @@ const getMessagesOfUser = ((req, res, next) => {
 
 
 const sendMessage = (req, res) => {
-  // const { messageData } = req.body;
-  // const parsedMessageData = JSON.parse(messageData);
   const {text, from, to, location} = req.body;
-  // console.log(location)
-  // const
-  // console.log(parsedMessageData);
-  // console.log(req.files);
-  // const { text, moduleID, user, to, location } = req.body;
-  // const pasredLocation = JSON.parse(location);
-  // console.log(pasredLocation);
-  // const { course, module, lesson } = JSON.parse(location);
-
-  // console.log(text, moduleID, user, to);
-  // console.log(req.files);
-  // const filesToSend = req.files.map((file) => {
-  //   const updatedPath = file.path.replace('public', 'https://api.sova-courses.site');
-  //   // const updatedPath = file.path.replace('public', 'http://localhost:3000');
-  //   return {...file, path: updatedPath};
-  // });
-
-  // console.log(filesToSend);
-
-
-
   Conversation.findOne({members: {$all: [from, to]}, location: location})
   .then((foundConvo) => {
     const message = {user: from, to: to, text: text, files:[]};
-
-    // function uploadToS3(file) {
-    //   // const file = req.file;
-    //   const sendFileCommand = new PutObjectCommand({
-    //     Bucket: process.env.BUCKET_NAME,
-    //     Key: file.originalname,
-    //     Body: file.buffer,
-    //     ContentType: file.mimetype
-    //   });
-
-    //   return getSignedUrl(s3, sendFileCommand, {
-    //     expiresIn: 90
-    //   })
-    // }
-
-    // console.log(foundConvo);
-  //   // console.log(foundConvo);
-    // const newMessage = {user: from, to: to, text: text, files: filesToSend};
-  //   // console.log(newMessage);
-  //   // // const foundConvo = doc.pop();
-  //   // // console.log(doc);
     if(foundConvo) {
       foundConvo.messages.push(message);
       foundConvo.save();
@@ -148,29 +137,10 @@ const sendMessage = (req, res) => {
         return res.status(201).send(newConvo.messages[newConvo.messages.length - 1]);
       })
     }
-  //   // return Conversation.create({members: [user, to], location: location})
-  //   // .then((createdConvo) => {
-  //   //   createdConvo.location = pasredLocation;
-  //   //   // console.log(createdConvo);
-  //   //   createdConvo.messages.push(newMessage);
-  //   //   createdConvo.save();
-  //   //   return res.status(201).send({ convo: foundConvo, lastMessage: newMessage });
-  //   //   // if(!convo) {
-  //   //   //   return;
-  //   //   // }
-  //   //   // // console.log('convo exists');
-  //   //   // return Message.create({text: text, user: user, to: to, module: moduleID, conversation: convo, files: req.files})
-  //   //   // .then((message) =>{
-  //   //   //   if(!message) {
-  //   //   //     return; //process error
-  //   //   //   }
-  //   //   //   return res.status(201).send(message);
-  //   //   // });
-  //   // })
   })
 };
 
-const sendFileInMessage = (req, res) => {
+const sendFileInMessage = (req, res, next) => {
   const {messageData} = req.body;
   const parsedMessageData = JSON.parse(messageData);
   const { from, to, location } = parsedMessageData;
@@ -180,22 +150,48 @@ const sendFileInMessage = (req, res) => {
   
 
 
+  const putCommand = new PutObjectCommand({
+    Bucket: process.env.BUCKET_NAME,
+    Key: req.file.originalname,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype
+  });
+
+  s3.send(putCommand)
+  .then((data) => {
+    // console.log(data);
+      Conversation.findOne({members: {$all: [from, to]}, location: location})
+      .then((foundConvo) => {
+        if(foundConvo) {
+          foundConvo.messages.push(message);
+          foundConvo.save();
+          return res.status(201).send(foundConvo.messages[foundConvo.messages.length - 1])
+        } else {
+          return Conversation.create({members: [from, to], location: location, messages: [message]})
+          .then((createdConvo) => {
+            return res.status(201).send(createdConvo.messages[createdConvo.messages.length - 1])
+          })
+        }
+      })  
+  })
+  .catch((err) => {
+    // next({codeStatus: 500, message: "Что-то пошло не так, попробуйте другой файл"})
+  })
 
 
-
-  Conversation.findOne({members: {$all: [from, to]}, location: location})
-  .then((foundConvo) => {
-    if(foundConvo) {
-      foundConvo.messages.push(message);
-      foundConvo.save();
-      return res.status(201).send(foundConvo.messages[foundConvo.messages.length - 1])
-    } else {
-      return Conversation.create({members: [from, to], location: location, messages: [message]})
-      .then((createdConvo) => {
-        return res.status(201).send(createdConvo.messages[createdConvo.messages.length - 1])
-      })
-    }
-  })  
+  // Conversation.findOne({members: {$all: [from, to]}, location: location})
+  // .then((foundConvo) => {
+  //   if(foundConvo) {
+  //     foundConvo.messages.push(message);
+  //     foundConvo.save();
+  //     return res.status(201).send(foundConvo.messages[foundConvo.messages.length - 1])
+  //   } else {
+  //     return Conversation.create({members: [from, to], location: location, messages: [message]})
+  //     .then((createdConvo) => {
+  //       return res.status(201).send(createdConvo.messages[createdConvo.messages.length - 1])
+  //     })
+  //   }
+  // })  
 };
 
 const getMessageFile = (req, res) => {
