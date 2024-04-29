e544727b091d7c088a1afded189b800294ef58aa
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-04-29 20:49:22 +0300

    testing upload

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,42 +1,72 @@
 const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const S3 = require("aws-sdk/clients/s3");
 
 //s3 initiation
-const s3 = new S3Client({
-  region: process.env.BUCKET_REGION,
+const s3Client = new S3({
+  signatureVersion: "v4",
   credentials: {
     accessKeyId: process.env.ACCESS_KEY,
     secretAccessKey: process.env.SECRET_KEY,
   },
+  region: process.env.BUCKET_REGION,
   endpoint: "https://storage.yandexcloud.net",
 })
+// const s3 = new S3Client({
+//   region: process.env.BUCKET_REGION,
+//   credentials: {
+//     accessKeyId: process.env.ACCESS_KEY,
+//     secretAccessKey: process.env.SECRET_KEY,
+//   },
+//   endpoint: "https://storage.yandexcloud.net",
+//   // signatur
+// })
 
 const imageUpload = (req, res) => {
   console.log(req.body);
 };
 
 const getUploadUrl = (req, res, next) => {
-  console.log(req.body);
-  const { name } = req.body;
-  const putCommand = new PutObjectCommand({
+  // console.log(req.body);
+  // const { name } = req.body;
+  // console.log(name);
+  // const putCommand = new PutObjectCommand({
+  //   Bucket: process.env.BUCKET_NAME,
+  //   Key: name,
+  // })
+  // const bucketParams = {
+  //   Bucket: process.env.BUCKET_NAME,
+  //   Key: name,
+  //   Expires: 120,
+  // }
+
+  const url = s3Client.getSignedUrl('putObject', {
     Bucket: process.env.BUCKET_NAME,
-    Key: name,
-    // ContentType: "application/json"
-  })
-  getSignedUrl(s3, putCommand, {
-    expiresIn: 120,
-    
-  })
-  .then((url) => {
-    if (typeof url !== 'string') {
-      throw new Error("Что-то пошло не так, попробуйте другой файл");
-    }
-    res.status(201).send({signedUrl: url});
-  })
-  .catch((err) => {
-    next({codeStatus: 500, message: err.message})
-    //process error
-  })
+    Key: `${req.body.name}`,
+    Expires: 120,
+    ContentType: req.body.type,
+  });
+
+  return res.status(201).send({url: url});
+
+  // console.log(url);
+
+  // getSignedUrl(s3, putCommand, {
+  //   expiresIn: 120,
+  // })
+  // .then((url) => {
+  //   if (typeof url !== 'string') {
+  //     throw new Error("Что-то пошло не так, попробуйте другой файл");
+  //   }
+  //   res.status(201).send({url: url});
+  // })
+  // .catch((err) => {
+  //   next({codeStatus: 500, message: err.message})
+  //   //process error
+  // })
 };
 
 const fileUpload = (req, res) => {
